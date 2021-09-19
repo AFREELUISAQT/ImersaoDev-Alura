e624991a31b5d2d2de280ec510cbcf0c59b0e822
afreeluisaqt <afreeluisaqt@gmail.com>
2021-09-19 11:41:06 -0300

    Aula1

---
+++ Aula1/calculando.js
@@ -0,0 +1,12 @@
+var ola = "Olá! ";
+var nome = "Eu sou a LuisaQT";
+var descripcao = "Minha Nota alcançada na Aula 01 da Imersão Dev da Alura é: ";
+var relatorio = ", correspondendo a uma porcentagem de: ";
+
+var notaAula1 = 10;
+const porcentagem = 100;
+
+var notaFinal = (notaAula1 * porcentagem) / 10;
+
+console.log(ola + "Bem-vind@s! " + nome);
+console.log(descripcao + notaAula1 + relatorio + notaFinal + "%");
---
+++ README.md
@@ -0,0 +1,6 @@
+# Imersao-Dev
+
+#Práticas dos Desafíos da Imersão Dev 3era Edição, da Alura.
+
+10 Aulas de Conteúdo sobre Html, Css e JavaScript.
+
